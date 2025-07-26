a722c8fe0b589b367375f39ed5289915bc394c82
Omeragturk <agturk37@gmail.com>
2025-07-26 17:48:37 +0300

    .

--- MovieWebsite.Infrastructure/Context/AppDbContext.cs
+++ MovieWebsite.Infrastructure/Context/AppDbContext.cs
@@ -184,7 +184,7 @@ namespace MovieWebsite.Infrastructure.Context
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server="");
+            optionsBuilder.UseSqlServer("Server=");
 
         }
 
